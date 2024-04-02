254ddfa12db19742389bfa33e57a93a215fcc7e2
minh.tran <finalleo258@gmail.com>
2024-04-03 00:37:19 +0700

    Initialize project

---
+++ src/test/java/vn/minhtranq/sampleservice/SampleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.minhtranq.sampleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
