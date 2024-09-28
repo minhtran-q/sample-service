70c2eed4f1354c33f90e74682d05641515b51212
minh.tran <finalleo258@gmail.com>
2024-09-28 20:33:03 +0700

    update controller

--- src/main/java/vn/minhtranq/sampleservice/controller/GoldController.java
+++ src/main/java/vn/minhtranq/sampleservice/controller/GoldController.java
@@ -1,16 +1,49 @@
 package vn.minhtranq.sampleservice.controller;
 
-import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import vn.minhtranq.sampleservice.model.request.GoldRequest;
 
 @RestController
+@RequestMapping("/gold/v1")
 public class GoldController {
 
-  @PostMapping("/test")
-  public void test(@RequestBody @Valid GoldRequest goldRequest) {
-    // do something
+  @GetMapping
+  public GoldRequestList getAllGold() {
+    return goldService.getAllGold();
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<GoldResponse> getGoldById(@PathVariable Long id) {
+    GoldResponse goldResponse = goldService.getGoldById(id);
+    if (goldResponse != null) {
+      return ResponseEntity.ok(goldResponse);
+    } else {
+      return ResponseEntity.notFound().build();
+    }
+  }
+
+  @PostMapping
+  public ResponseEntity<GoldResponse> createGold(@RequestBody GoldRequest goldRequest) {
+    GoldResponse goldResponse = goldService.createGold(goldRequest);
+    URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+        .buildAndExpand(goldResponse.getId()).toUri();
+    return ResponseEntity.created(location).body(goldResponse);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<GoldResponse> updateGold(@PathVariable Long id, @RequestBody GoldRequest goldRequest) {
+    GoldResponse goldResponse = goldService.updateGold(id, goldRequest);
+    if (goldResponse != null) {
+      return ResponseEntity.ok(goldResponse);
+    } else {
+      return ResponseEntity.notFound().build();
+    }
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteGold(@PathVariable Long id) {
+    goldService.deleteGold(id);
+    return ResponseEntity.ok().build();
   }
 }
